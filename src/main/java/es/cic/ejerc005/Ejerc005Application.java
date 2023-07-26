a1980a6edf19163122885e38fe78f8b65b5420e5
curso00 <curso00@nomail.com>
2023-07-26 12:52:54 +0200

    Capas de Controller y Service, con test de integración y unitario rotos

---
+++ src/main/java/es/cic/ejerc005/Ejerc005Application.java
@@ -0,0 +1,15 @@
+package es.cic.ejerc005;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejerc005Application {
+
+	// Esto es un comentario
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Ejerc005Application.class, args);
+	}
+
+}
---
+++ src/test/java/es/cic/ejerc005/Ejerc005ApplicationTests.java
@@ -0,0 +1,14 @@
+package es.cic.ejerc005;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejerc005ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
